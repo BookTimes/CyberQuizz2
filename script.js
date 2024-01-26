4450379eec0b90c7ccdbec886fdc8db8a896b398
Mahadev Maneesh <maha040963@sisharjah.com>
2024-01-26 16:44:34 +0400

    first commit

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const sub = document.getElementById("subm");
 var cont = document.getElementById("let").innerText;
 var q = 0;
-const qnLog = [];
-const anLog = [2, 0, 1, 1, 3, 0, 3, 2, 0, 1, 3, 0, 2, 1, 1, 2, 2, 3, 1, 0];
+var qnLog = [];
+var anLog = [2, 0, 1, 1, 3, 0, 3, 2, 0, 1, 3, 0, 2, 1, 1, 2, 2, 3, 1, 0];
+// const correctAnswers = [0, 0, 0, 1, 2, 0, 1, 1, 2, 1];
+
 var h = true;
+var jv = true;
+
 // http://jsonblob.com/1197622298986536960
 var nam;
 var clss;
@@ -43,142 +47,141 @@ const instruct = `
 </div>
 `;
 var i = 0;
+var qns = [
+  {
+    qn: "A hardware device/software program that filters all the packets of data that comes through a network, the internet, etc. is called:",
+    options: ["Cookies", "Antivirus", "Firewall", "Protocol"],
+  },
+  {
+    qn: "Internet and WWW (World Wide Web) are considered the same. Choose Suitable Option for Given Statement",
+    options: ["True", "False", "Partially True & False", "None"],
+  },
+  {
+    qn: "What is Netiquette?",
+    options: [
+      "Treat Others as badly as They Treat You",
+      "Be Polite Online & Others will do the Same",
+      "Etiquette for Netflix",
+      "None of the Above",
+    ],
+  },
+  {
+    qn: "Which of the following typically keeps tabs on every online activity the victim engages in, compiles all the data in the background, and sends it to a third party?",
+    options: ["Cookies", "Spyware", "Adware", "All of the Above"],
+  },
+  {
+    qn: "What tasks can a Computer perform?",
+    options: [
+      "Type Documents & send Emails",
+      "Internet Browsing",
+      "Store & Retrieve Info",
+      "All of the Above",
+    ],
+  },
+  {
+    qn: '"Data encryption is used to ensure confidentiality" Choose Suitable Option for Given Statement:',
+    options: ["True", "False", "None", "Partially True & False"],
+  },
+  {
+    qn: "Which of the following are Programming Languages?",
+    options: ["Python", "C++", "Java", "All of the Above"],
+  },
+  {
+    qn: "Which of these is something you should do if you are cyber bullied?",
+    options: [
+      "Don't Open/Read their Messages",
+      "Tell your Internet Service Provider",
+      "Tell a Trusted Adult about it",
+      "Keep it to yourself",
+    ],
+  },
+  {
+    qn: "HTTPS is a Protocol that means:",
+    options: [
+      "HyperText Transfer Protocol Secure",
+      "HyperTransfer Text Protocol Safety",
+      "HastyText Traversal Protocol Secure",
+      "None of the Above",
+    ],
+  },
+  {
+    qn: "In the World of CyberSecurity, What is CIA?",
+    options: [
+      "Commercial Investment Authority",
+      "Confidentiality, Integrity, and Availability",
+      "Central Investigative Authority",
+      "None of the Above",
+    ],
+  },
+  {
+    qn: "Copying of a web-page or website & storing that copy for the purpose of speeding up subsequent access is called:",
+    options: ["Downloading", "Browsing", "File Swapping", "Caching"],
+  },
+  {
+    qn: "In which Year was the Information Technology Act passed?",
+    options: ["2000", "1978", "2021", "2015"],
+  },
+  {
+    qn: "Cybercrime is criminal activity that targets/uses a computer, a computer network or a networked device & which doesn't include:",
+    options: ["Phishing", "Privacy Violation", "Bank Theft", "Data Breach"],
+  },
+  {
+    qn: "From the Given Statements, Which of the following IS not TRUE about HTTP?",
+    options: [
+      "URL Scheme Begins with 'http://'",
+      "Data is Encrypted before transmission",
+      "Prone to Eavesdropping & Data interception",
+      "Data is sent in Plain Text",
+    ],
+  },
+  {
+    qn: "From the Passwords listed below, which is the Strongest?",
+    options: ["!798$", "Aw12q3", "@1A5p$#2", "All of the Above"],
+  },
+  {
+    qn: "A VPN establishes a Digital Connection between the Computer & a Remote Server owned by the VPN Provider. What does VPN stand for?",
+    options: [
+      "Vast Private Network",
+      "Virtual Public Network",
+      "Virtual Private Network",
+      "Void Public Net",
+    ],
+  },
+  {
+    qn: "Which of the following is a Type of Cyber Security?",
+    options: [
+      "Application Security",
+      "Cloud Security",
+      "Network Security",
+      "All of the Above",
+    ],
+  },
+  {
+    qn: "A Black Hat Hacker Maliciously uses Hacking skills to breach confidential data. A White Hat Hacker performs Ethical Hacking for Common Good. Then, Which of these statements about a Grey Hat Hacker is False?",
+    options: [
+      "Their Behaviour is Unpredictable",
+      "Can use their skills for Common Good",
+      "Might become a Potential Threat at any time",
+      "No Similarity with Black Hat & White Hat Hackers",
+    ],
+  },
+  {
+    qn: "Which of the following is known as Malicious software?",
+    options: ["Illegal Ware", "Badware", "Malware", "Malicious Ware"],
+  },
+  {
+    qn: "Yahoo.inc Vs Akash Arora is a Famous Case relating to Which of the following?",
+    options: [
+      "Cyber Squatting",
+      "Cyber Stalking",
+      "Cyber Defamation",
+      "None of the Above",
+    ],
+  },
+];
+expectedLevelShufflingPncOnly(qns, anLog);
 
 const askQn = function (i) {
-  const qns = [
-    {
-      qn: "A hardware device/software program that filters all the packets of data that comes through a network, the internet, etc. is called:",
-      options: ["Cookies", "Antivirus", "Firewall", "Protocol"],
-    },
-    {
-      qn: "Internet and WWW (World Wide Web) are considered the same. Choose Suitable Option for Given Statement",
-      options: ["True", "False", "Partially True & False", "None"],
-    },
-    {
-      qn: "What is Netiquette?",
-      options: [
-        "Treat Others as badly as They Treat You",
-        "Be Polite Online & Others will do the Same",
-        "Etiquette for Netflix",
-        "None of the Above",
-      ],
-    },
-    {
-      qn: "Which of the following typically keeps tabs on every online activity the victim engages in, compiles all the data in the background, and sends it to a third party?",
-      options: ["Cookies", "Spyware", "Adware", "All of the Above"],
-    },
-    {
-      qn: "What tasks can a Computer perform?",
-      options: [
-        "Type Documents & send Emails",
-        "Internet Browsing",
-        "Store & Retrieve Info",
-        "All of the Above",
-      ],
-    },
-    {
-      qn: '"Data encryption is used to ensure confidentiality" Choose Suitable Option for Given Statement:',
-      options: ["True", "False", "None", "Partially True & False"],
-    },
-    {
-      qn: "Which of the following are Programming Languages?",
-      options: ["Python", "C++", "Java", "All of the Above"],
-    },
-    {
-      qn: "Which of these is something you should do if you are cyber bullied?",
-      options: [
-        "Don't Open/Read their Messages",
-        "Tell your Internet Service Provider",
-        "Tell a Trusted Adult about it",
-        "Keep it to yourself",
-      ],
-    },
-    {
-      qn: "HTTPS is a Protocol that means:",
-      options: [
-        "HyperText Transfer Protocol Secure",
-        "HyperTransfer Text Protocol Safety",
-        "HastyText Traversal Protocol Secure",
-        "None of the Above",
-      ],
-    },
-    {
-      qn: "In the World of CyberSecurity, What is CIA?",
-      options: [
-        "Commercial Investment Authority",
-        "Confidentiality, Integrity, and Availability",
-        "Central Investigative Authority",
-        "None of the Above",
-      ],
-    },
-    {
-      qn: "Copying of a web-page or website & storing that copy for the purpose of speeding up subsequent access is called:",
-      options: ["Downloading", "Browsing", "File Swapping", "Caching"],
-    },
-    {
-      qn: "In which Year was the Information Technology Act passed?",
-      options: ["2000", "1978", "2021", "2015"],
-    },
-    {
-      qn: "Cybercrime is criminal activity that targets/uses a computer, a computer network or a networked device & which doesn't include:",
-      options: ["Phishing", "Privacy Violation", "Bank Theft", "Data Breach"],
-    },
-    {
-      qn: "From the Given Statements, Which of the following IS not TRUE about HTTP?",
-      options: [
-        "URL Scheme Begins with 'http://'",
-        "Data is Encrypted before transmission",
-        "Prone to Eavesdropping & Data interception",
-        "Data is sent in Plain Text",
-      ],
-    },
-    {
-      qn: "From the Passwords listed below, which is the Strongest?",
-      options: ["!798$", "Aw12q3", "@1A5p$#2", "All of the Above"],
-    },
-    {
-      qn: "A VPN establishes a Digital Connection between the Computer & a Remote Server owned by the VPN Provider. What does VPN stand for?",
-      options: [
-        "Vast Private Network",
-        "Virtual Public Network",
-        "Virtual Private Network",
-        "Void Public Net",
-      ],
-    },
-    {
-      qn: "Which of the following is a Type of Cyber Security?",
-      options: [
-        "Application Security",
-        "Cloud Security",
-        "Network Security",
-        "All of the Above",
-      ],
-    },
-    {
-      qn: "A Black Hat Hacker Maliciously uses Hacking skills to breach confidential data. A White Hat Hacker performs Ethical Hacking for Common Good. Then, Which of these statements about a Grey Hat Hacker is False?",
-      options: [
-        "Their Behaviour is Unpredictable",
-        "Can use their skills for Common Good",
-        "Might become a Potential Threat at any time",
-        "No Similarity with Black Hat & White Hat Hackers",
-      ],
-    },
-    {
-      qn: "Which of the following is known as Malicious software?",
-      options: ["Illegal Ware", "Badware", "Malware", "Malicious Ware"],
-    },
-    {
-      qn: "Yahoo.inc Vs Akash Arora is a Famous Case relating to Which of the following?",
-      options: [
-        "Cyber Squatting",
-        "Cyber Stalking",
-        "Cyber Defamation",
-        "None of the Above",
-      ],
-    },
-  ];
-
-  // expectedLevelShufflingPncOnly(qns, anLog);
   var w = 0;
   document.getElementById("let").innerHTML = ` <div class="row ">
     <div class="col-8">
@@ -223,6 +226,7 @@ const askQn = function (i) {
 <button class="qns">19</button>
 <button class="qns">20</button>
 
+
     </div>
 
     <div class="px-3  justify-content-between d-flex gap-4 ">
@@ -247,7 +251,6 @@ const askQn = function (i) {
   //     askQn(Number(button.innerText) - 1);
   //   });
   // });
-
   if (i < 20) {
     i += 1;
 
@@ -266,24 +269,34 @@ document.getElementsByTagName("html")[0].addEventListener("mouseleave", () => {
   const j = setInterval(() => {
     q += 1;
     console.log(q);
-    if (q == 3) {
-      document.getElementsByClassName("overlay")[0].innerHTML =
-        "<h1>Disqualified</h1>";
-      h = false;
+    if (q == 3 && jv == true) {
       document.getElementById("let").innerHTML = "";
+      out("http://jsonblob.com/api/1200131415496056832");
+      document.getElementsByClassName(
+        "overlay"
+      )[0].innerHTML = `<div class="position-absolute hid d-flex justify-content-center align-items-center flex-column overlay p-5">
+        <h1 class=" fw-bold text-white">
+            You are disqualified reload to start again or keep the window open till end of given time to keep the score, you may ask a cyber squad volunteer for help
+        </h1>
+        <img class="w-25"  src="https://ugokawaii.com/wp-content/uploads/2022/10/redcard.gif">
+    </div>`;
+      h = false;
+      jv = false;
     }
   }, 1000);
-  document.getElementsByClassName("hid")[0].classList.remove("hid");
-  document
-    .getElementsByTagName("html")[0]
-    .addEventListener("mouseenter", () => {
-      if (h == true) {
-        document.getElementsByClassName("overlay")[0].classList.add("hid");
-      }
+  if (jv == true) {
+    document.getElementsByClassName("hid")[0].classList.remove("hid");
+    document
+      .getElementsByTagName("html")[0]
+      .addEventListener("mouseenter", () => {
+        if (h == true) {
+          document.getElementsByClassName("overlay")[0].classList.add("hid");
+        }
 
-      q = 0;
-      clearInterval(j);
-    });
+        q = 0;
+        clearInterval(j);
+      });
+  }
 });
 
 const dataSend = function () {
@@ -329,12 +342,12 @@ const get = function () {
       console.error("Fetch error:", error);
     });
 };
-const out = function () {
+const out = function (url = "https://jsonblob.com/api/1197622298986536960") {
   // Create a new XMLHttpRequest object
   var xhr = new XMLHttpRequest();
 
   // Specify the URL of the API endpoint you want to update
-  var url = "https://jsonblob.com/api/1197622298986536960"; // Example URL, replace with your API endpoint
+  // Example URL, replace with your API endpoint
 
   // Data to be updated (replace with your actual data)
 
@@ -369,6 +382,9 @@ const out = function () {
   xhr.send(jsonData);
 };
 const endExam = function () {
+  jv = false;
+  h = false;
+
   dataSend();
   document.getElementById(
     "let"
@@ -379,7 +395,7 @@ const endExam = function () {
 };
 
 const j = setInterval(() => {
-  var endTime = new Date("2024-01-23T16:50:00");
+  var endTime = new Date("2024-01-24T08:05:00");
 
   var currentTime = new Date();
 
@@ -406,19 +422,43 @@ function expectedLevelShufflingPncOnly(arr1, arr2) {
     throw new Error("Arrays must have the same length");
   }
 
-  // Generate a random permutation of indices
   const permutation = Array.from(
     { length: arr1.length },
     (_, index) => index
   ).sort(() => Math.random() - 0.5);
-
-  // Apply the same permutation to both arrays
   const shuffledArr1 = permutation.map((index) => arr1[index]);
   const shuffledArr2 = permutation.map((index) => arr2[index]);
-
-  // Update the original arrays with the shuffled values
+  console.log(arr1);
+  console.log(arr2);
+  console.log(shuffledArr1);
+  console.log(shuffledArr2);
   arr1.length = 0;
   arr2.length = 0;
   arr1.push(...shuffledArr1);
   arr2.push(...shuffledArr2);
 }
+var oldX = window.screenX,
+  oldY = window.screenY;
+var interval = setInterval(function () {
+  if (oldX != window.screenX || oldY != window.screenY) {
+    if (jv == true) {
+      document.getElementById("let").innerHTML = "";
+
+      document.getElementsByClassName(
+        "overlay"
+      )[0].innerHTML = `<div class="position-absolute hid d-flex justify-content-center align-items-center flex-column overlay p-5">
+        <h1 class=" fw-bold text-white">
+            You are disqualified reload to start again or keep the window open till end of given time to keep the score, you may ask a cyber squad volunteer for help
+        </h1>
+        <img class="w-25"  src="https://ugokawaii.com/wp-content/uploads/2022/10/redcard.gif">
+    </div>`;
+      out("http://jsonblob.com/api/1200131415496056832");
+      h = false;
+      jv = false;
+    }
+  } else {
+    console.log("not moved!");
+  }
+  oldX = window.screenX;
+  oldY = window.screenY;
+}, 500);
